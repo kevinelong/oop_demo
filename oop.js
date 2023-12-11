c254fd04997e0f4a066223c96fda60106154b81e
kevinelong <kevinelong@gmail.com>
2023-12-11 14:02:44 -0800

    store

--- oop.js
+++ oop.js
@@ -8,6 +8,51 @@
 //Coffee Maker, Toaster, Child, Dog, input actions, output actions, attributes.
 
 //What Classes of objects do you see in the world?
+//PRODUCT/ITEM
+//SHOP/SELLER/VENDOR/WEBSITE/RETAILER/SHOPPING-CENTER
+//SEARCH-ENGINER
+//CUSTOMER
+//PHOTO/IMAGE
+//INFO/SPECS/FEATURES/PRICES/MODEL/BRAND
+//BRAND
+//PAYMENT
+//TRANSACTION
+
+//RELATIONSHIPS BETWEEN CLASSES
+// IS A - e.g. Cat is an Animal (uncommon kind of relationship)
+// HAS A - e.g. Person has a Pet. (common kind)
+
+/*
+Online shopping is a form of electronic commerce which allows consumers to 
+directly buy goods or services from a seller over the Internet using a web 
+browser or a mobile app. Consumers find a product of interest by visiting the 
+website of the retailer directly or by searching among alternative vendors 
+using a shopping search engine, which displays the same product's availability 
+and pricing at different e-retailers. As of 2020, customers can shop online 
+using a range of different computers and devices, including desktop computers, 
+laptops, tablet computers and smartphones.
+
+An online shop evokes the physical analogy of buying products or services at a 
+regular "brick-and-mortar" retailer or shopping center; the process is called 
+business-to-consumer (B2C) online shopping. When an online store is set up to 
+enable businesses to buy from another businesses, the process is called 
+business-to-business (B2B) online shopping. A typical online store enables the 
+customer to browse the firm's range of products and services, view photos or 
+images of the products, along with information about the product 
+specifications, features and prices.
+
+Online stores usually enable shoppers to use "search" features to find specific
+ models, brands or items. Online customers must have access to the Internet and
+  a valid method of payment in order to complete a transaction, such as a 
+  credit card, an Interac-enabled debit card, or a service such as PayPal. For 
+  physical products (e.g., paperback books or clothes), the e-tailer ships the 
+  products to the customer; for digital products, such as digital audio files 
+  of songs or software, the e-tailer usually sends the file to the customer 
+  over the Internet. The largest of these online retailing corporations are 
+  Alibaba, Amazon.com, and eBay.[1] 
+*/
+
+
 
 
 class Cat {
---
+++ store.js
@@ -0,0 +1,66 @@
+class Store{
+    //current order is a cart
+    constructor(productList){
+        this.productList = productList;
+        this.cart = new Order();
+    }
+    show(){
+        this.productList.forEach((p, i)=> {
+            console.log(i, p.show())
+        });
+    }
+    add(i, quantity=1){
+        this.cart.itemList.push(new LineItem(this.productList[i], quantity))
+    }
+}
+
+class Product{
+    constructor(name, price = 0, attributes = {}){
+        this.name = name;
+        this.price = price;
+        this.atrributes = attributes;
+    }
+    show(){
+        return `NAME: ${this.name} PRICE: ${this.price}`
+    }
+}
+
+class Customer{
+    //...
+}
+
+class LineItem{
+    constructor(product, quantity){
+        this.product = product;
+        this.quantity = quantity;
+    }
+}
+
+class Order{ //CART IS AN INCOMPLETE ORDER
+    constructor(){
+        this.customer = new Customer();
+        this.itemList = [];
+    }
+    show(){
+        let total = 0;
+        this.itemList.forEach(item=>{
+            let ext = item.quantity * item.product.price;
+            total += ext;
+            console.log(item.quantity, item.product.show(), ext)
+        });
+        console.log(`TOTAL: ${total}`)
+    }
+}
+
+//TEST
+const s = new Store([
+    new Product("paper", 5.00),
+    new Product("pencil", 1.00),
+]);
+
+s.show()
+
+s.add(0,2)
+s.add(1, 12)
+
+s.cart.show();
