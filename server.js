03d84c41c7b008e98bcbd5e979832e37fcc94b62
kevinelong <kevinelong@gmail.com>
2023-12-15 09:20:40 -0800

    create user

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ function getUserData(cb) {
         SELECT username, jsontext
         FROM profiles
         JOIN datastore
-        ON profile_id = profiles.id;`, cb);
+        ON profile_id = profiles.id;
+`, cb);
     });
 }
 function setUserData(username, jsontext, cb) {
@@ -31,13 +32,26 @@ function setUserData(username, jsontext, cb) {
         WHERE profile_id = (
             SELECT id FROM profiles 
             WHERE username = '${username}'
-        );`
+        );
+`
         console.log(sql);
         con.query(sql, cb);
     });
 }
+function createUser(username, cb) {
+    con.connect(function (err) {
+        con.query(`INSERT INTO profiles (username) VALUES ('${username}');`, 
+        (err, result, fields) => {
+            con.query(`
+            INSERT INTO datastore (jsontext, profile_id) 
+            VALUES ('{}', '${result.insertId}');
+            `, cb);
+        });
+    });
+}
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Please use: /api/userdata')
 })
 
 app.get('/api/userdata', (req, res) => {
@@ -47,10 +61,19 @@ app.get('/api/userdata', (req, res) => {
 app.post('/api/userdata', (req, res) => {
     console.log(req.body.color);
 
-    setUserData(req.body.username, encodeURIComponent(JSON.stringify(req.body)), e=>{
+    setUserData(req.body.username, encodeURIComponent(JSON.stringify(req.body)), e => {
         res.send("SUCCESS")
     });
 });
+
+
+app.post('/api/user', (req, res) => {
+    createUser(req.body.username, (err, result, fields) => {
+        console.log(err, result, fields)
+        res.send("SUCCESS")
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
