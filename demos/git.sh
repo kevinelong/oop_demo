632757f8eb6013c7d29225653aafebf4a03533eb
kevinelong <kevinelong@gmail.com>
2023-12-18 11:53:38 -0800

    git

---
+++ demos/git.sh
@@ -0,0 +1,16 @@
+git status        # Show status of current git repository
+git branch b1     # create a new git branch named b1
+git checkout main #switch to the main branch
+git merge b1      # merge b1 into the current branch
+git diff b1       # compare current branch and branch b1
+git pull          #fetch and merge from remote
+git log           # shows history
+git checkout b1   #switches to branch b1
+git add .         #add new files
+git commit -m 'message' #commit locally
+git commit -a -m 'message' # add *AND* commit locally
+git push          #push to origin e.g. github.com
+
+# PULL REQUESTS AKA PRs let folks know you have a branch that can be merged into main
+# Create them at GITHUB.COM WHERE ALL CAN SEE THEM
+# they can be comment on and optionally merged or abandoned
